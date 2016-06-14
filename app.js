277e46ecb31b524d64a89f7a0e3401bd652f54f0
Mahedi <mahedi0244@gmail.com>
2016-06-14 15:45:27 -0400

    javascript inspirational is added

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var exphbs  = require('express-handlebars');
 
 var app = express();
+var quotes = ["You are inspired when you find inspiration", "If you seek inspiration, look for it", "I am always inspired by inspirational quotes"];
 
 app.use(express.static(__dirname + '/public'));
 app.engine('handlebars', exphbs({defaultLayout:'main'}));
@@ -16,10 +17,18 @@ app.get('/', function (req, res) {
 });
 app.get('/about', function (req, res) {   
 	res.render('about'); 
-}); 
+});
+/* 
 app.get('/inspiration', function (req, res) {   
 	res.render('inspiration'); 
 });
+*/
+app.get('/inspiration', function (req, res) {
+	var randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
+	res.render('inspiration', { inspirational: randomQuote });
+  
+});
+
 app.listen(3000, function () {   
 	console.log('Inspiration app listening on port 3000!'); 
 });
